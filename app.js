7053e094740495eaacb5905f7cf90fe79139998f
Wojciech <wojtek5552@vp.pl>
2023-08-17 21:36:51 +0200

    Remove unused index.js and its route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var magnetoRouter = require('./routes/magneto');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', indexRouter);
 app.use('/magneto', magnetoRouter);
 
 // catch 404 and forward to error handler
